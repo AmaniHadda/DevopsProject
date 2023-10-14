6b96df9020edbf952749d40a91efc0638cb27320
baher <baher.ammari@esprit.tn>
2023-10-14 22:26:51 +0100

    testProduct

---
+++ src/main/java/tn/esprit/devops_project/entities/Product.java
@@ -0,0 +1,35 @@
+package tn.esprit.devops_project.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+
+public class Product implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    Long idProduct;
+
+    String title;
+
+    float price;
+
+    int quantity;
+    @Enumerated(EnumType.STRING)
+    ProductCategory category;
+    @ManyToOne
+    @JsonIgnore
+    Stock stock;
+}
