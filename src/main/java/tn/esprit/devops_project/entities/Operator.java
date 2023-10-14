2930bd205e39e2a63f3bc708a430dbf9373bdb06
haykelmhadhbi <mhadhbi.haikel@gmail.com>
2023-10-14 22:51:24 +0100

    operatortest

---
+++ src/main/java/tn/esprit/devops_project/entities/Operator.java
@@ -0,0 +1,48 @@
+package tn.esprit.devops_project.entities;
+
+import java.io.Serializable;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Operator implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@NonNull
+	Long idOperateur;
+	@NonNull
+	String fname;
+	@NonNull
+	String lname;
+	@NonNull
+	String password;
+	@OneToMany
+	@JsonIgnore
+	Set<Invoice> invoices;
+
+
+
+
+}
+
+
+
+
---
+++ src/main/java/tn/esprit/devops_project/repositories/OperatorRepository.java
@@ -0,0 +1,11 @@
+package tn.esprit.devops_project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import tn.esprit.devops_project.entities.Operator;
+
+
+
+public interface OperatorRepository extends JpaRepository<Operator, Long> {
+}
