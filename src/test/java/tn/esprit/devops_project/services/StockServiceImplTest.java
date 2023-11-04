fae676658586f232dc37d42609737723dbc3aee8
Amani Hadda <amani.hadda@esprit.tn>
2023-11-04 15:01:44 +0100

    Update StockServiceImplTest.java

--- src/test/java/tn/esprit/devops_project/services/StockServiceImplTest.java
+++ src/test/java/tn/esprit/devops_project/services/StockServiceImplTest.java
@@ -7,6 +7,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import tn.esprit.devops_project.entities.Stock;
 import tn.esprit.devops_project.repositories.StockRepository;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.HashSet;
 import java.util.List;
