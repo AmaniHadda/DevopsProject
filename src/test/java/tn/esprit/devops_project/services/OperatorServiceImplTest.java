e4c10f4dc276eae2c88da03400f85f8654bb6ea0
Amani Hadda <amani.hadda@esprit.tn>
2023-11-04 15:02:21 +0100

    Update OperatorServiceImplTest.java

--- src/test/java/tn/esprit/devops_project/services/OperatorServiceImplTest.java
+++ src/test/java/tn/esprit/devops_project/services/OperatorServiceImplTest.java
@@ -13,6 +13,7 @@ import tn.esprit.devops_project.entities.Operator;
 import tn.esprit.devops_project.repositories.OperatorRepository;
 import tn.esprit.devops_project.services.OperatorServiceImpl;
 import static org.assertj.core.api.Assertions.assertThat ;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.ArrayList;
 import java.util.List;
